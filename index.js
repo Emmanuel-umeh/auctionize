abeae5771ed06f9c44ba189b80588002d17baf6e
Emmanuel <Emmanuelsumeh@gmail.com>
2019-09-07 09:11:14 -0700

    loading functionality

--- index.js
+++ index.js
@@ -1,116 +1,114 @@
-const contractSource = '
-contract MyAuction =
+const contractSource = 'contract MyAuction =
 
-  record product = 
-    { creatorAddress : address,
-      url            : string,
-      nameofProduct  : string,
-      currentPrice   : int,
-      sold           : bool }
+record product = 
+  { creatorAddress : address,
+    url            : string,
+    nameofProduct  : string,
+    currentPrice   : int,
+    sold           : bool }
 
-  record state = 
-    { products : map(int, product),
-      productLength : int,
-      bidders : map(int,address),
-      bidderLength : int }
-    
-  entrypoint init() = 
-    { products = {}, 
-      productLength = 0,
-      bidders = {},
-      bidderLength = 0 }
+record state = 
+  { products : map(int, product),
+    productLength : int,
+    bidders : map(int,address),
+    bidderLength : int }
   
-  entrypoint getProduct(index:int) : product = 
-    switch(Map.lookup(index, state.products))
-      None => abort("Product does not exist with this index")
-      Some(x) => x  
-    
-  stateful entrypoint registerProduct(url' : string, nameofProduct': string, currentPrice': int) =
-    let product = { creatorAddress = Call.caller, url = url', nameofProduct = nameofProduct', currentPrice = currentPrice', sold = false}  
-    let index = getProductLength() + 1 
-    put(state{products[index] = product, productLength  = index})
-    
+entrypoint init() = 
+  { products = {}, 
+    productLength = 0,
+    bidders = {},
+    bidderLength = 0 }
 
-  entrypoint getProductLength() : int = 
-    state.productLength
+entrypoint getProduct(index:int) : product = 
+  switch(Map.lookup(index, state.products))
+    None => abort("Product does not exist with this index")
+    Some(x) => x  
+  
+stateful entrypoint registerProduct(url' : string, nameofProduct': string, currentPrice': int) =
+  let product = { creatorAddress = Call.caller, url = url', nameofProduct = nameofProduct', currentPrice = currentPrice', sold = false}  
+  let index = getProductLength() + 1 
+  put(state{products[index] = product, productLength  = index})
+  
+
+entrypoint getProductLength() : int = 
+  state.productLength
+
+
+
+//bid functionality
+
+stateful entrypoint bid(index: int) =
+  let product = getProduct(index)
+  let addresses = Call.caller
+  let updatedBid = Call.value
+  let contractBalance = getContractBalance()
+  put(state{bidders[index]= addresses})
+  
+  biddingSecurity()
   
   
+  if(product.sold == true)
+    abort("product has been sold")
+  //first bid
+  if(Call.value > product.currentPrice &&  contractBalance == 0)
+    Chain.spend(Contract.address, Call.value)
+    
+    
+    //second bid
+  if(Call.value > product.currentPrice && contractBalance != 0)
+    let previousbidder = getBidderAddress(index-1)
+    Chain.spend(previousbidder,Contract.balance)
+   
+  elif(Call.value < updatedBid)
+    abort("your bid is lower than the current bid")
+  else
+    abort("you need to enter a value higher than 0 ")
+  let updatedProduct = state.products{ [index].currentPrice = updatedBid}
+  let index = getBidderLength() + 1
   
-  //bid functionality
   
-  stateful entrypoint bid(index: int) =
-    let product = getProduct(index)
-    let addresses = Call.caller
-    let updatedBid = Call.value
-    let contractBalance = getContractBalance()
-    put(state{bidders[index]= addresses})
+  put(state { products = updatedProduct})
     
-    biddingSecurity()
-    
-    
-    if(product.sold == true)
-      abort("product has been sold")
-    //first bid
-    if(Call.value > product.currentPrice &&  contractBalance == 0)
-      Chain.spend(Contract.address, Call.value)
-      
-      
-      //second bid
-    if(Call.value > product.currentPrice && contractBalance != 0)
-      let previousbidder = getBidderAddress(index-1)
-      Chain.spend(previousbidder,Contract.balance)
-     
-    elif(Call.value < updatedBid)
-      abort("your bid is lower than the current bid")
-    else
-      abort("you need to enter a value higher than 0 ")
-    let updatedProduct = state.products{ [index].currentPrice = updatedBid}
-    let index = getBidderLength() + 1
-    
-    
-    put(state { products = updatedProduct})
-      
-    put(state{bidderLength = index})
-    
-    
-  //length of registered bidders
-  entrypoint getBidderLength() : int = 
-    state.bidderLength   
-    
-    
-  //stores address of registered bidsers
+  put(state{bidderLength = index})
   
-  entrypoint getBidderAddress(index:int) = 
-    switch(Map.lookup(index, state.bidders))
-      None => abort("bidder does not exist with this index")
-      Some(x) => x 
   
- // stateful entrypoint bidders(name:string)  = 
-   // let bidder = { bidderAddress = Call.caller, updatedPrice = Call.value, name = name }
-    //let index = getBidderLength() + 1
-    //put(state{bidders[index] = bidders, bidderLength  = index})
-    //put(state{bidders[index] = bidders, bidders  = bidder})
-    
-  stateful entrypoint closeBid(index : int) = 
-    put(state{products[index].sold = true})
-    let product = getProduct(index)
-    let total = Contract.balance
-    let creatorsAddress = product.creatorAddress
-    Chain.spend(creatorsAddress: address, product.currentPrice: int)
-    
-    
-  public entrypoint getContractBalance() : int =
-    Contract.balance
+//length of registered bidders
+entrypoint getBidderLength() : int = 
+  state.bidderLength   
   
-  public entrypoint getContractowner() : address =
-    Contract.creator
-    
-  stateful entrypoint biddingSecurity() = 
-    if(Call.caller == Contract.creator)
-      abort("you cannot bid on your own product")
-'; 
-const contactAddress = 'ct_tHnPWpfw12hsvqMnchTeqQAaHtCqjiEg7GLFEBokMA4Y81kA';
-var ProductArray = {};
+  
+//stores address of registered bidsers
+
+entrypoint getBidderAddress(index:int) = 
+  switch(Map.lookup(index, state.bidders))
+    None => abort("bidder does not exist with this index")
+    Some(x) => x 
+
+// stateful entrypoint bidders(name:string)  = 
+ // let bidder = { bidderAddress = Call.caller, updatedPrice = Call.value, name = name }
+  //let index = getBidderLength() + 1
+  //put(state{bidders[index] = bidders, bidderLength  = index})
+  //put(state{bidders[index] = bidders, bidders  = bidder})
+  
+stateful entrypoint closeBid(index : int) = 
+  put(state{products[index].sold = true})
+  let product = getProduct(index)
+  let total = Contract.balance
+  let creatorsAddress = product.creatorAddress
+  Chain.spend(creatorsAddress: address, product.currentPrice: int)
+  
+  
+public entrypoint getContractBalance() : int =
+  Contract.balance
+
+public entrypoint getContractowner() : address =
+  Contract.creator
+  
+stateful entrypoint biddingSecurity() = 
+  if(Call.caller == Contract.creator)
+    abort("you cannot bid on your own product")'; 
+const contactAddress = 'ct_wiVwt4Rts4v5UwAAhfCLjEPysdEHLx78vwXnAjiSXdMHRB7ed';
+var ProductArray = [];
 var client = null;
 var productLength = 0;
 
@@ -124,23 +122,39 @@ function renderProduct()
     var rendered = Mustache.render(template, {ProductArray});
     $('#productBody').html(rendered);
 }
+//Create a asynchronous read call for our smart contract
+async function callStatic(func, args) {
+  //Create a new contract instance that we can interact with
+  const contract = await client.getContractInstance(contractSource, {contractAddress});
+  //Make a call to get data of smart contract func, with specefied arguments
+  const calledGet = await contract.call(func, args, {callStatic: true}).catch(e => console.error(e));
+  //Make another call to decode the data received in first call
+  const decodedGet = await calledGet.decode().catch(e => console.error(e));
+
+  return decodedGet;
+}
 
 window.addEventListener('load', async () => {
-    $("#loader").show();
+    $('#loader').show();
 
     client = await Ae.Aepp();
 
-    const contract = await client.getContractInstance(contractSource, {contractAddress});
-    const calledGet = await contract.call('getProductLength', [], {callStatic : true}).catch(e => console.error(e));
-    console.log('calledGet', calledGet);
+    productLength = await callStatic('getProductLength', []); 
+    
+    for(let i = 1; i<= productLength; i++ ){
+        const product =  await callStatic("getProduct", [1]);
+    
 
-    const decodedGet = await calledGet.decode().catch(e => console.error(e));
-    console.log('decodedGet', decodedGet)
-
-
+    ProductArray.push({
+        name : product.name,
+        url : product.url,
+        index : ProductArray.length + 1,
+        productName : product.productName,
+        Price : product.price 
+    })
+}
     renderProduct();
-    $("#loader").hide();
-
+    $('#loader').hide();
 });
 
 // document.getElementById("bidButton").addEventListener('click', function(event){
