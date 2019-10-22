654997ea11d5d41708f83a965863d46c5c2102ce
Emmanuel <Emmanuelsumeh@gmail.com>
2019-10-22 16:26:01 -0700

    COntract

--- assets/js/loader.js
+++ assets/js/loader.js
@@ -1,6 +1,8 @@
-// window.onload = function() 
-//     {
-//         //display loader on page load 
-//         $('#loadings').fadeOut()
-//     }
+function hideLoader() {
+    $('#loading').hide();
+    $('#loader').fadeOut();
+}
 
+
+// Strongly recommended: Hide loader after 20 seconds, even if the page hasn't finished loading
+setTimeout(hideLoader, 20 * 1000);
