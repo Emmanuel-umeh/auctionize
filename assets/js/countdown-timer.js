393250324c3b978af6bb29b3b9134ccde9813e48
Emmanuel <Emmanuelsumeh@gmail.com>
2019-10-23 02:52:33 -0700

    timer

--- assets/js/countdown-timer.js
+++ assets/js/countdown-timer.js
@@ -2,7 +2,7 @@
 function makeTimer() {
 
   //		var endTime = new Date("29 April 2018 9:56:00 GMT+01:00");	
-    var endTime = new Date("29 April 2020 9:56:00 GMT+01:00");			
+  var endTime = new Date("29 October 2019 9:56:00 GMT+01:00");			
       endTime = (Date.parse(endTime) / 1000);
 
       var now = new Date();
@@ -41,44 +41,3 @@ function makeTimer() {
 
 
 
-// function getTimeRemaining(endtime) {
-//   var t = Date.parse(endtime) - Date.parse(new Date());
-//   var seconds = Math.floor((t / 1000) % 60);
-//   var minutes = Math.floor((t / 1000 / 60) % 60);
-//   var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-//   var days = Math.floor(t / (1000 * 60 * 60 * 24));
-//   return {
-//     'total': t,
-//     'days': days,
-//     'hours': hours,
-//     'minutes': minutes,
-//     'seconds': seconds
-//   };
-// }
-
-// function initializeClock(id, endtime) {
-//   var clock = document.getElementById(id);
-//   var daysSpan = clock.querySelector('.days');
-//   var hoursSpan = clock.querySelector('.hours');
-//   var minutesSpan = clock.querySelector('.minutes');
-//   var secondsSpan = clock.querySelector('.seconds');
-
-//   function updateClock() {
-//     var t = getTimeRemaining(endtime);
-
-//     daysSpan.innerHTML = t.days;
-//     hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
-//     minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-//     secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
-
-//     if (t.total <= 0) {
-//       clearInterval(timeinterval);
-//     }
-//   }
-
-//   updateClock();
-//   var timeinterval = setInterval(updateClock, 1000);
-// }
-
-// var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
-// initializeClock('clockdiv', deadline);
--- index.js
+++ index.js
@@ -240,7 +240,7 @@ $('#regButton').click(async function(){
     function makeTimer() {
 
       //		var endTime = new Date("29 April 2018 9:56:00 GMT+01:00");	
-        var endTime = new Date("29 April 2020 9:56:00 GMT+01:00");			
+        var endTime = new Date("29 October 2019 9:56:00 GMT+01:00");			
           endTime = (Date.parse(endTime) / 1000);
     
           var now = new Date();
