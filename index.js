ee771fad5b2b3b9bf4b30afcbd0e2cf49a477ce9
Emmanuel <Emmanuelsumeh@gmail.com>
2019-10-23 01:57:20 -0700

    COntract

--- index.js
+++ index.js
@@ -126,37 +126,11 @@ function renderProduct()
 {
     ProductArray = ProductArray.sort(function(a,b){return b.Price - a.Price})
     var template = $('#template').html();
+    
     Mustache.parse(template);
     var rendered = Mustache.render(template, {ProductArray});
 
-    function makeTimer() {
-
-      //		var endTime = new Date("29 April 2018 9:56:00 GMT+01:00");	
-        var endTime = new Date("29 April 2020 9:56:00 GMT+01:00");			
-          endTime = (Date.parse(endTime) / 1000);
     
-          var now = new Date();
-          now = (Date.parse(now) / 1000);
-    
-          var timeLeft = endTime - now;
-    
-          var days = Math.floor(timeLeft / 86400); 
-          var hours = Math.floor((timeLeft - (days * 86400)) / 3600);
-          var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600 )) / 60);
-          var seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)));
-      
-          if (hours < "10") { hours = "0" + hours; }
-          if (minutes < "10") { minutes = "0" + minutes; }
-          if (seconds < "10") { seconds = "0" + seconds; }
-    
-          $("#days").html(days + "<span>Days</span>");
-          $("#hours").html(hours + "<span>Hours</span>");
-          $("#minutes").html(minutes + "<span>Minutes</span>");
-          $("#seconds").html(seconds + "<span>Seconds</span>");		
-    
-      }
-    
-      setInterval(function() { makeTimer(); }, 1000);
   
 
     $('#productBody').html(rendered);
@@ -254,13 +228,43 @@ $("#productBody").on("click", ".bidButton", async function(event){
 
 $('#regButton').click(async function(){
   $("#loadings").show();
+
     var name =($('#sellerName').val()),
     price = parseInt(($('#regPrice').val()),10),
     url = ($('#regUrl').val()),
     productName = ($('#regName').val());
     description = ($('#productDescription').val());
     await contractCall('registerProduct', [url,productName,description,price])
-    console.log(name)
+    console.log(name),
+
+    function makeTimer() {
+
+      //		var endTime = new Date("29 April 2018 9:56:00 GMT+01:00");	
+        var endTime = new Date("29 April 2020 9:56:00 GMT+01:00");			
+          endTime = (Date.parse(endTime) / 1000);
+    
+          var now = new Date();
+          now = (Date.parse(now) / 1000);
+    
+          var timeLeft = endTime - now;
+    
+          var days = Math.floor(timeLeft / 86400); 
+          var hours = Math.floor((timeLeft - (days * 86400)) / 3600);
+          var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600 )) / 60);
+          var seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)));
+      
+          if (hours < "10") { hours = "0" + hours; }
+          if (minutes < "10") { minutes = "0" + minutes; }
+          if (seconds < "10") { seconds = "0" + seconds; }
+    
+          $("#days").html(days + "<span>Days</span>");
+          $("#hours").html(hours + "<span>Hours</span>");
+          $("#minutes").html(minutes + "<span>Minutes</span>");
+          $("#seconds").html(seconds + "<span>Seconds</span>");		
+    
+      }
+    
+      setInterval(function() { makeTimer(); }, 1000);
 
     
 
