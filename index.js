c5e9f0fd6b1a30e53da404b7ee5caff846d838d6
Emmanuel <Emmanuelsumeh@gmail.com>
2019-10-23 11:14:57 -0700

    Index.js

--- index.js
+++ index.js
@@ -169,6 +169,34 @@ window.addEventListener('load', async () => {
     const product =  await callStatic('getProduct', [i]);
     
     console.log("for loop reached")
+    function makeTimer() {
+
+      //		var endTime = new Date("29 April 2018 9:56:00 GMT+01:00");	
+        var endTime = new Date("29 October 2019 9:56:00 GMT+01:00");			
+          endTime = (Date.parse(endTime) / 1000);
+    
+          var now = new Date();
+          now = (Date.parse(now) / 1000);
+    
+          var timeLeft = endTime - now;
+    
+          var days = Math.floor(timeLeft / 86400); 
+          var hours = Math.floor((timeLeft - (days * 86400)) / 3600);
+          var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600 )) / 60);
+          var seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)));
+      
+          if (hours < "10") { hours = "0" + hours; }
+          if (minutes < "10") { minutes = "0" + minutes; }
+          if (seconds < "10") { seconds = "0" + seconds; }
+    
+          days = $("#days").html(days + "<span>Days</span>");
+          hours = $("#hours").html(hours + "<span>Hours</span>");
+          minutes = $("#minutes").html(minutes + "<span>Minutes</span>");
+          seconds = $("#seconds").html(seconds + "<span>Seconds</span>");		
+    
+      }
+    
+      setInterval(function() { makeTimer(); }, 1000);
 
     ProductArray.push({
       sold : product.sold,
@@ -176,7 +204,11 @@ window.addEventListener('load', async () => {
       index : i,
       productNames : product.nameofProduct,
       Price : product.currentPrice ,
-      review : product.reviews
+      review : product.reviews,
+      days : days,
+      hours : hours,
+      minutes : minutes,
+      seconds : seconds
   })
 }
   renderProduct();
